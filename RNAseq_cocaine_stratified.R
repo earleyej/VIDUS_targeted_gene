69f71c6c4ede64d19683f0467a850a2941299052
Eric Earley <eearley@rti.org>
2023-11-13 10:19:04 -0500

    Update RNAseq_cocaine_stratified.R

--- RNAseq_cocaine_stratified.R
+++ RNAseq_cocaine_stratified.R
@@ -246,3 +246,48 @@ filtered.vidus.gene.dds <- count.filter(vidus.gene.dds, count.cutoff = 10,
 dim(filtered.vidus.gene.dds) 
 # estimating size factors
 filtered.vidus.gene.dds <- estimateSizeFactors(filtered.vidus.gene.dds)
+
+
+
+# save spot
+#save.image("save.spot.RData")
+load("save.spot.RData")
+
+
+
+
+######################################
+#### Differential Gene Expression ####
+######################################
+
+# DGE model - BASE + absolute.cell.proportions
+common.vars <- c('female', 'ageatint',
+    "RNA_Quality_Score", 'PC1', 'PC2', 'PC3', 'PC4', 'PC5',
+    "cd4Tcells","Bcells","granulocytes","Monocytes","T.cells.CD8")
+contrast.var <- "hiv"
+vidus.full.formula <- paste0("~", paste0(c(contrast.var, common.vars), collapse=" + "))
+design(filtered.vidus.gene.dds) <- as.formula(vidus.full.formula)
+design(filtered.vidus.gene.dds)
+
+# perform the regression
+vidus.fit <- DESeq(filtered.vidus.gene.dds, test = "Wald",
+        fitType = "parametric", sfType = "ratio", betaPrior = F,
+        parallel = F)
+
+# Access results
+resultsNames(vidus.fit)
+hiv.results <- DESeq2::results(vidus.fit, name = "hiv_1_vs_0", alpha = 0.05, cooksCutoff = Inf)
+
+
+
+##########################
+#### apeGLM shrinkage ####
+##########################
+#apply apeGLM shrinkage to fold changes
+# this takes a long time, consider parallelizing
+hiv.shrunk.results <- hiv.results
+hiv.shrunk.results <- lfcShrink(vidus.fit, res = hiv.shrunk.results, 
+    coef = "hiv_1_vs_0", type = "apeglm", parallel = F)
+
+
+
