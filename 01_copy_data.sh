7dd52b907fb354a5c1e67036550af4dfca677362
Eric Earley <eearley@rti.org>
2023-11-15 14:12:52 -0500

    Update 01_copy_data.sh

--- 01_copy_data.sh
+++ 01_copy_data.sh
@@ -2,12 +2,11 @@
 aws s3 cp s3://rti-hiv/differential_gene_expression/vidus/data/phenotype/vidus_hiv_acquisition_phenotype_variables.txt.gz ./
 
 # technical co-variates
-aws s3 cp s3://rti-hiv/differential_gene_expression/vidus/data/phenotype/vidus_rna_seq_technical_variables.txt.gz \
-    /shared/vidus/
+aws s3 cp s3://rti-hiv/differential_gene_expression/vidus/data/phenotype/vidus_rna_seq_technical_variables.txt.gz ./
 
 # genotype PCs
 aws s3 cp s3://rti-hiv/differential_gene_expression/vidus/data/phenotype/eigenstrat/vidus_hiv_acquisition_vl_suppressed_samples_top10_genotype_pcs.txt ./
-gunzip ./vidus*variables.txt.gz
+#gunzip ./vidus*variables.txt.gz
 
 # cell type proportions
 aws s3 cp s3://rti-hiv/differential_gene_expression/vidus/data/gene_expression/deconvolution/cibersortx/hiv_status_candidate_gene_dge_cell_type_proportions_lm22.txt ./
