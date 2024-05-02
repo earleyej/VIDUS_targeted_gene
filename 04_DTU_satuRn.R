a2988e096e593869082841ed56d6839e7b43d121
Eric Earley <eearley@rti.org>
2024-05-02 13:41:55 -0400

    Update 04_DTU_satuRn.R

--- 04_DTU_satuRn.R
+++ 04_DTU_satuRn.R
@@ -24,7 +24,8 @@ suppressMessages(library(SummarizedExperiment))
 suppressMessages(library(ggplot2))
 suppressMessages(library(DEXSeq))
 suppressMessages(library(stageR))
-
+library(BiocParallel)
+options(MulticoreParam=MulticoreParam(workers=6))
 
 
 #### Load Data ####
@@ -40,8 +41,6 @@ tx = assay(filtered.vidus.tx.dds)
 # it has already been filtered for low-count transcripts
 # see code 03a_DTE_prepare_data.R
 
-
-
 # load transcript - gene table that I made in 03a_DTE_prepare_data.R
 txInfo = readRDS("/vidus/gencode_v28_tx_id_name_map.rds")
 # remove version number from transcript ID
@@ -49,31 +48,54 @@ txInfo = data.frame(txInfo)
 txInfo$TRANSCRIPTID = gsub("\\..*$","",txInfo$TRANSCRIPTID)
 rownames(tx) = gsub("\\..*$","",rownames(tx))
 
+# restrict to just the targeted genes for the HIV paper
+pull <- c("CD44",
+          "CX3CR1",
+          "EPSTI1",
+          "HCP5",
+          "IFI44L",
+          "IFIT3",
+          "IFITM1",
+          "MX1",
+          "NLRC5",
+          "PARP9",
+          "PLSCR1",
+          "RASSF3",
+          "RIN2",
+          "RSAD2",
+          "TAP1",
+          "TAP2",
+          "TNF",
+          "TNIP3")
+txInfo = txInfo[txInfo$GENENAME %in% pull,]
+dim(tx) #60K
+tx = tx[rownames(tx) %in% txInfo$TRANSCRIPTID,]
+dim(tx) #119
 
 # Remove genes that only have 1 transcript
 txInfo <- txInfo[txInfo$TRANSCRIPTID %in% rownames(tx), ]
-dim(txInfo) #61,523
+dim(txInfo) #119
 txInfo <- subset(txInfo, 
                  duplicated(GENENAME) | duplicated(GENENAME, fromLast = TRUE))
-dim(txInfo) #53,406
+dim(txInfo) #116
 tx <- tx[which(
   rownames(tx) %in% txInfo$TRANSCRIPTID), ]
-dim(tx) #53,320
+dim(tx) #116
 
 # make the txInfo and tx colinear
 txInfo <- txInfo[match(rownames(tx), txInfo$TRANSCRIPTID), ]
 
-# there are still lonely transcripts in the count data. Remove them
-txInfo <- subset(txInfo, duplicated(GENENAME) | duplicated(GENENAME, fromLast = TRUE))
-tx <- tx[which(rownames(tx) %in% txInfo$TRANSCRIPTID), ] # 53,314
 
 
-
-# Subset the pheno file 
+# Subset the pheno file columns
 # convert age to 5 year bins
 pheno$age.bin = cut(pheno$ageatint,breaks=5)
 # Age, sex, RNA quality (RIN), 5 deconvolution-derived cell class proportions, and the top 5 Principal Components
 pheno.analysis = pheno[,c("iid","hiv","female","age.bin","RNA_Quality_Score","cd4Tcells","Bcells","granulocytes","Monocytes","T.cells.CD8",paste0("PC",c(1:5)))]
+#### TESTING ###
+pheno.analysis$hiv = ifelse(pheno.analysis$hiv == 0,"N","Y")
+#### END ####
+
 
 # create design matrix from the pheno file
 # All three main functions of satuRn require a SummarizedExperiment object as an input class
@@ -84,34 +106,102 @@ sumExp <- SummarizedExperiment::SummarizedExperiment(
     colData = pheno.analysis,
     rowData = txInfo
 )
-metadata(sumExp)$formula <- ~ 0 + hiv + female + as.factor(colData(sumExp)$age.bin) + RNA_Quality_Score + cd4Tcells + Bcells + granulocytes + Monocytes + T.cells.CD8 + PC1 + PC2 + PC3 + PC4 + PC5
-
-
+#metadata(sumExp)$formula <- ~ 0 + hiv + female + as.factor(colData(sumExp)$age.bin) + RNA_Quality_Score + cd4Tcells + Bcells + granulocytes + Monocytes + T.cells.CD8 + PC1 + PC2 + PC3 + PC4 + PC5
+metadata(sumExp)$formula <- ~ 0 + as.factor(colData(sumExp)$hiv)
 
 
 # Fit quasi-binomial generalized linear model
-# ~5 min.
+# ~5 min. for the full transcriptome
 system.time({
 sumExp <- satuRn::fitDTU(
-    object = sumExp,
-    formula = ~ 0 + hiv + female + as.factor(colData(sumExp)$age.bin) + RNA_Quality_Score + cd4Tcells + Bcells + granulocytes + Monocytes + T.cells.CD8 + PC1 + PC2 + PC3 + PC4 + PC5,
+    object = sumExp,   
     parallel = FALSE,
+    formula = ~ 0 + hiv,
     BPPARAM = BiocParallel::bpparam(),
     verbose = TRUE
 )
 })
-
+#formula = ~ 0 + as.factor(hiv) + female + as.factor(colData(sumExp)$age.bin) + RNA_Quality_Score + cd4Tcells + Bcells + granulocytes + Monocytes + T.cells.CD8 + PC1 + PC2 + PC3 + PC4 + PC5,
 
 
 # set up the contrast matrix
-design <- model.matrix(~ 0 + hiv + female + as.factor(colData(sumExp)$age.bin) + RNA_Quality_Score + cd4Tcells + Bcells + granulocytes + Monocytes + T.cells.CD8 + PC1 + PC2 + PC3 + PC4 + PC5, 
-                       data=pheno.analysis) 
-L = contr.treatment(pheno.analysis$hiv)
+# using limma
+group <- as.factor(pheno.analysis$hiv)
+design <- model.matrix(~ 0 + group) # construct design matrix
+#colnames(design) <- c(0,1)
+L <- limma::makeContrasts(
+    Contrast1 = "groupN - groupY",
+    levels = design
+)
+
+#manually
+#L = data.frame("Contrast1" = c(-1,1))
+#rownames(L) = c(0,1)
+
 
 # Perform the contrast
-system.time({
 sumExp <- satuRn::testDTU(
     object = sumExp,
+    contrasts = L,
+    diagplot1 = TRUE,
+    diagplot2 = TRUE,
+    sort = FALSE
+)
+
+
+# save spot
+save.image("/vidus/DTU_saturn_2024_05_02.RData")
+
+
+
+# Results
+head(rowData(sumExp)[["fitDTUResult_Contrast1"]])
+
+#
+
+
+
+
+
+
+
+
+
+
+
+
+## Testing below using smaller subsets of data
+# 1,000 rows = 37 seconds
+# 5,000 rows = 165 seconds
+# 20,000 rows = 642 seconds (10 min)
+tx.sub = tx[c(1:20000),]
+txInfo.sub = txInfo[c(1:20000),]
+# remove lonely transcripts
+txInfo.sub <- subset(txInfo.sub, 
+                 duplicated(gene_id) | duplicated(gene_id, fromLast = TRUE))
+tx.sub <- tx[which(
+  rownames(tx.sub) %in% txInfo.sub$isoform_id), ]
+
+# create object
+sumExp.sub <- SummarizedExperiment::SummarizedExperiment(
+    assays = list(counts = tx.sub),
+    colData = pheno.analysis,
+    rowData = txInfo.sub
+)
+metadata(sumExp.sub)$formula <- ~ 0 + hiv 
+# fit simple model
+sumExp.sub <- satuRn::fitDTU(
+    object = sumExp.sub,
+    formula = ~ 0 + hiv,
+    parallel = FALSE,
+    BPPARAM = BiocParallel::bpparam(),
+    verbose = TRUE
+)
+# perform contrast
+L = contr.treatment(pheno.analysis$hiv)
+system.time({
+sumExp.sub <- satuRn::testDTU(
+    object = sumExp.sub,
     contrasts = L,
     diagplot1 = TRUE,
     diagplot2 = TRUE,
